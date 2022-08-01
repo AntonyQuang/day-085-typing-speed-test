c6b16474aec07a2bf64455559fed219daf1f799e
Antony Quang <antony.quang@googlemail.com>
2022-08-01 22:23:56 +0100

    Change from KeyRelease to KeyPress. Add score updater. Add most recent score. Add milestone for BackSpace function. Changed word source

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 import tkinter as tk
 from tkinter.ttk import *
 import sv_ttk
-import pandas
 import random
 
-#TODO 1. Set out GUI
 
 # ------- Tkinter Setup -------- #
 
 root = tk.Tk()
 root.title("Typing Speed Test")
-root.geometry("800x600+0+0")
+root.geometry("500x400+0+0")
 root.config(padx=50, pady=50)
 sv_ttk.use_light_theme()
 
 # ------- Class Setup --------- #
 
-nato_df = pandas.read_csv("nato_phonetic_alphabet.csv")
-word_list = [row.code for (index, row) in nato_df.iterrows()]
+with open("random-words.txt") as random_words:
+    word_list = random_words.read().split(",")
+
+word_list.pop(0)
+#
+# nato_df = pandas.read_csv("nato_phonetic_alphabet.csv")
+# word_list = [row.code for (index, row) in nato_df.iterrows()]
 
 
 class TypingTest:
@@ -30,17 +33,35 @@ class TypingTest:
         self.position = 0
         self.word_number = 0
         self.cpm_correct_total = 0
+        self.milestone = 0
 
 
         # Title
-        self.title = Label(root, text="Typing Speed Test")
-        self.title.grid(row=0, column=0, columnspan=2)
+        self.title = Label(root, text="Typing Speed Test", padding=5)
+        self.title.grid(row=0, column=0, columnspan=5)
+
+        # Most Recent Scores
+        self.recent_body = Frame(root, padding=5)
+        self.recent_body.grid(row=1, column=0, columnspan=5)
+        self.recent_body.config(borderwidth=2, relief="solid")
+
+        self.recent_scores = Label(self.recent_body, text="Most recent score:")
+        self.recent_scores.grid(row=0, column=0)
+
+        self.recent_cpm_label = Label(self.recent_body, text="Corrected CPM: ")
+        self.recent_cpm_label.grid(row=1, column=0)
+        self.recent_cpm = Entry(self.recent_body, width=5, justify="center")
+        self.recent_cpm.grid(row=1, column=1)
+
+        self.recent_wpm_label = Label(self.recent_body, text="Corrected WPM: ")
+        self.recent_wpm_label.grid(row=1, column=2)
+        self.recent_wpm = Entry(self.recent_body, width=5, justify="center")
+        self.recent_wpm.grid(row=1, column=3)
 
         # Body
         self.body = Frame(root)
-        self.body.grid(row=1, column=0, columnspan=2)
-        self.body.config(padding=4)
-        self.body["borderwidth"]=10
+        self.body.grid(row=2, column=0, columnspan=5)
+        self.body.config(padding=4, borderwidth=4, relief='solid')
 
         # Stats
         self.cpm_label = Label(self.body, text="Corrected CPM: ")
@@ -62,7 +83,7 @@ class TypingTest:
         self.timer.grid(row=0, column=5)
 
         # Words
-        self.words = tk.Text(self.body, width=50, height=5, wrap="word")
+        self.words = tk.Text(self.body, width=50, height=3, wrap="word")
         self.words.grid(row=1, column=0, columnspan=6, pady=5)
         random.shuffle(word_list)
 
@@ -73,38 +94,46 @@ class TypingTest:
         self.string_of_words = self.words.get("1.0", "end")
 
         # Typing Zone
-        self.typer = Entry(self.body, width=66, justify="center")
+        self.typer = Entry(self.body, width=50, justify="center")
         self.typer.grid(row=2, column=0, columnspan=6)
         self.typer.focus()
-        self.typer.bind("<KeyRelease>", self.start)
+        self.typer.bind("<KeyPress>", self.start)
 
         # Buttons
         self.quit_btn = Button(root, text="Quit", command=root.destroy)
         self.quit_btn.grid(row=3, column=0)
 
         self.restart_btn = Button(root, text="Restart", command=self.reset)
-        self.restart_btn.grid(row=3, column=1)
+        self.restart_btn.grid(row=3, column=4)
 
     # ----------- LOGIC -------------- #
 
     def start(self, event):
+        self.milestone = 0
         self.time = 0
         self.position = 0
         self.word_number = 0
         self.cpm_correct_total = 0
         self.countdown()
+        # This checks the first key pressed
         self.input_handler(event)
-        self.typer.bind("<KeyRelease>", self.input_handler)
+        # This turns on the checking for the subsequent keys pressed
+        self.typer.bind("<KeyPress>", self.input_handler)
 
     def countdown(self):
         if self.time == self.time_limit:
+            # When the game is over, prevent the user from typing and save the score
             self.typer.config(state="disabled")
-            self.typer.unbind()
+            self.typer.unbind("<KeyPress>")
+            self.save_score()
         elif self.time > self.time_limit:
+            # This sets things to normal when the restart button is pressed
             self.typer.config(state="normal")
         else:
+            # This is the normal countdown
             self.time += 1
             time_remaining = self.time_limit - self.time
+            # This displays the time left
             self.timer.delete(0, "end")
             self.timer.insert(0, f"{time_remaining}")
             # After 1 second, start again
@@ -112,6 +141,7 @@ class TypingTest:
 
     def reset(self):
         # Delete Entries
+        self.typer.config(state="normal")
         self.time = self.time_limit * 2
         self.timer.delete(0, "end")
         self.wpm.delete(0, "end")
@@ -120,92 +150,119 @@ class TypingTest:
         self.words.tag_delete("correct_char", "incorrect_char")
         self.words.delete("1.0", "end")
 
-
         # Back to starting values
         self.timer.insert(0, f"{self.time_limit}")
         self.wpm.insert(0, "?")
         self.cpm.insert(0, "?")
         self.typer.focus()
-        self.typer.bind("<KeyRelease>", self.start)
+        self.typer.config(state="normal")
+        self.typer.bind("<KeyPress>", self.start)
+        self.typer.bind("<BackSpace>", self.backspace)
         self.words.config(state="normal")
+        # Creating a random word list and string of all words put together.
+        # You need both forms to do different functions.
         random.shuffle(word_list)
+        # The 'words' object is useful for word to word gameplay
         for word in word_list:
             word = word.lower()
             self.words.insert("end", f"{word} ")
+
+        # String of words is useful for the character to character gameplay
         self.string_of_words = self.words.get("1.0", "end")
         self.words.config(state="disabled")
 
     def input_handler(self, event):
-        if event.char:
-            self.check_char(event)
-        elif event.keysym == "BackSpace":
+        if event.keysym == "BackSpace":
             self.backspace()
+        elif event.char:
+            self.check_char(event)
+        else:
+            # This deals if someone tries to press non-character keys
+            return
 
     def check_char(self, event):
-        char_typed = event.char
+        char_typed = event.char.lower()
+        print(char_typed)
         if char_typed == " ":
             self.check_word()
+
         elif char_typed == self.string_of_words[self.position]:
+            # Compares the character typed to the particular character in the string of words
+            # If it is the same, it highlights the character blue
             self.words.tag_add("correct_char", f"1.{self.position}", f"1.{self.position + 1}")
-            self.words.tag_config("correct_char", background="blue")
+            self.words.tag_config("correct_char", background="blue", foreground="white")
         else:
+            # If it is different, it highlights the character red
             self.words.tag_add("incorrect_char", f"1.{self.position}", f"1.{self.position + 1}")
-            self.words.tag_config("incorrect_char", background="red")
+            self.words.tag_config("incorrect_char", background="red", foreground="white")
         self.position += 1
 
     def backspace(self):
-        if self.typer.get():
+        # Conditions for backspace: When you typed the first character in a word
+        # The second condition accounts for the very first word
+        print("backspace")
+        if self.position > self.milestone+1 or self.position == 1:
             self.position -= 1
             self.words.tag_remove("correct_char", f"1.{self.position}", f"1.{self.position + 1}")
             self.words.tag_remove("incorrect_char", f"1.{self.position}", f"1.{self.position + 1}")
 
     def check_word(self):
+        # Grabs the correct answer
         word_to_check_against = word_list[self.word_number].lower()
-        input_word = self.typer.get().removesuffix(" ").lower()
-        print(self.position)
+        # Grabs the attempt
+        input_word = self.typer.get().removeprefix(" ").lower()
 
-        if input_word == word_to_check_against:
-            print("correct")
-
+        if input_word != word_to_check_against:
+            # Removes all highlights of the word attempted, even if they wrote a word longer than it should be
             self.words.tag_remove("correct_char", f"1.{self.position - len(input_word)}", f"1.{self.position}")
             self.words.tag_remove("incorrect_char", f"1.{self.position - len(input_word)}", f"1.{self.position}")
 
-        else:
-            print("incorrect")
-            self.words.tag_remove("correct_char", f"1.{self.position - len(input_word)}", f"1.{self.position}")
-            self.words.tag_remove("incorrect_char", f"1.{self.position - len(input_word)}", f"1.{self.position}")
-
+        # Go back one because of the space
         self.position = -1
 
+        # Calculate new position based on how many words have been attempted
         for i in range(self.word_number+1):
             self.position += len(word_list[i]) + 1
 
+        #self.milestone is purely for the backspace method
+        self.milestone = self.position * 1
+
         if input_word == word_to_check_against:
+            # Highlight the correct word blue (by highlighting the characters
+            # from beginning of word to current position)
             self.words.tag_add("correct_char", f"1.{self.position - len(word_list[self.word_number])}", f"1.{self.position}")
+            self.words.tag_config("correct_char", background="blue", foreground="white")
+            self.update_score()
 
-            self.cpm_correct_total += len(word_list[self.word_number])
-            self.cpm_score = self.cpm_correct_total/(self.time_limit/60)
-            self.cpm.delete(0, "end")
-            self.cpm.insert(0, f"{self.cpm_score}")
-
-            self.wpm_score = self.cpm_score/5
-            self.wpm.delete(0, "end")
-            self.wpm.insert(0, f"{self.wpm_score}")
         else:
             self.words.tag_add("incorrect_char", f"1.{self.position - len(word_list[self.word_number])}",
                                f"1.{self.position}")
+            self.words.tag_config("incorrect_char", background="red", foreground="white")
 
+        self.words.see(f"1.{self.position + 50}")
         self.typer.delete(0, "end")
+
         self.word_number += 1
-        print(self.position)
+
+    def update_score(self):
+        self.cpm_correct_total += len(word_list[self.word_number])
+        self.cpm_score = self.cpm_correct_total / (self.time_limit / 60)
+        self.cpm.delete(0, "end")
+        self.cpm.insert(0, f"{'{:.1f}'.format(self.cpm_score)}")
+
+        self.wpm_score = self.cpm_score / 5
+        self.wpm.delete(0, "end")
+        self.wpm.insert(0, f"{'{:.1f}'.format(self.wpm_score)}")
+
+    def save_score(self):
+        self.recent_cpm.delete(0, "end")
+        self.recent_cpm.insert(0, f"{'{:.1f}'.format(self.cpm_score)}")
+
+        self.recent_wpm.delete(0, "end")
+        self.recent_wpm.insert(0, f"{'{:.1f}'.format(self.wpm_score)}")
 
 
 
 typing = TypingTest()
 root.mainloop()
 
-#TODO 2. Get a set of random words
-
-#TODO 4. Matching thing (on space)
-
-#TODO 5. Count score
