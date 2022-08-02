ff4d69248dd1722c8c024fec2e785f86153e4811
Antony Quang <antony.quang@googlemail.com>
2022-08-02 21:25:03 +0100

    Change colours, sizes and spacing

--- main.py
+++ main.py
@@ -3,25 +3,24 @@ from tkinter.ttk import *
 import sv_ttk
 import random
 
-
 # ------- Tkinter Setup -------- #
 
 root = tk.Tk()
 root.title("Typing Speed Test")
-root.geometry("500x400+0+0")
-root.config(padx=50, pady=50)
+root.geometry("510x500+0+0")
+root.config(padx=40, pady=40, background="#DDD2E6")
 sv_ttk.use_light_theme()
+background = Style()
+background.configure("P.TButton", highlightcolor="#DDD2E6")
 
-# ------- Class Setup --------- #
+# ------- Getting words ------- #
 
 with open("random-words.txt") as random_words:
     word_list = random_words.read().split(",")
-
 word_list.pop(0)
-#
-# nato_df = pandas.read_csv("nato_phonetic_alphabet.csv")
-# word_list = [row.code for (index, row) in nato_df.iterrows()]
 
+
+# ------- Class Setup --------- #
 
 class TypingTest:
     def __init__(self):
@@ -35,37 +34,40 @@ class TypingTest:
         self.cpm_correct_total = 0
         self.milestone = 0
 
-
         # Title
         self.title = Label(root, text="Typing Speed Test", padding=5)
         self.title.grid(row=0, column=0, columnspan=5)
+        self.title.config(font=('Calibri', 20, "bold"), background="#DDD2E6")
 
         # Most Recent Scores
-        self.recent_body = Frame(root, padding=5)
-        self.recent_body.grid(row=1, column=0, columnspan=5)
+        self.recent_frame = Frame(root)
+        self.recent_frame.grid(row=1, column=0, columnspan=5, pady=20, padx=20)
+
+        self.recent_body = Frame(self.recent_frame, padding=5)
+        self.recent_body.grid(row=0, column=0)
         self.recent_body.config(borderwidth=2, relief="solid")
 
         self.recent_scores = Label(self.recent_body, text="Most recent score:")
-        self.recent_scores.grid(row=0, column=0)
+        self.recent_scores.grid(row=0, column=0, columnspan=2, pady=5, sticky="W", padx=(5, 0))
 
         self.recent_cpm_label = Label(self.recent_body, text="Corrected CPM: ")
-        self.recent_cpm_label.grid(row=1, column=0)
+        self.recent_cpm_label.grid(row=1, column=0, padx=(20, 0))
         self.recent_cpm = Entry(self.recent_body, width=5, justify="center")
-        self.recent_cpm.grid(row=1, column=1)
+        self.recent_cpm.grid(row=1, column=1, padx=(0, 20))
 
         self.recent_wpm_label = Label(self.recent_body, text="Corrected WPM: ")
-        self.recent_wpm_label.grid(row=1, column=2)
+        self.recent_wpm_label.grid(row=1, column=3, padx=(14, 0))
         self.recent_wpm = Entry(self.recent_body, width=5, justify="center")
-        self.recent_wpm.grid(row=1, column=3)
+        self.recent_wpm.grid(row=1, column=5, padx=(0, 20))
 
         # Body
         self.body = Frame(root)
         self.body.grid(row=2, column=0, columnspan=5)
-        self.body.config(padding=4, borderwidth=4, relief='solid')
+        self.body.config(borderwidth=4, relief='solid')
 
         # Stats
         self.cpm_label = Label(self.body, text="Corrected CPM: ")
-        self.cpm_label.grid(row=0, column=0)
+        self.cpm_label.grid(row=0, column=0, padx=(10, 0))
         self.cpm = Entry(self.body, width=5, justify="center")
         self.cpm.insert(0, "?")
         self.cpm.grid(row=0, column=1)
@@ -80,7 +82,7 @@ class TypingTest:
         self.timer_label.grid(row=0, column=4)
         self.timer = Entry(self.body, width=5, justify="center")
         self.timer.insert(0, "60")
-        self.timer.grid(row=0, column=5)
+        self.timer.grid(row=0, column=5, pady=10, padx=(0, 10))
 
         # Words
         self.words = tk.Text(self.body, width=50, height=3, wrap="word")
@@ -94,17 +96,17 @@ class TypingTest:
         self.string_of_words = self.words.get("1.0", "end")
 
         # Typing Zone
-        self.typer = Entry(self.body, width=50, justify="center")
-        self.typer.grid(row=2, column=0, columnspan=6)
+        self.typer = Entry(self.body, width=49, justify="center")
+        self.typer.grid(row=2, column=0, columnspan=6, pady=(15, 20), padx=10)
         self.typer.focus()
         self.typer.bind("<KeyPress>", self.start)
 
         # Buttons
-        self.quit_btn = Button(root, text="Quit", command=root.destroy)
-        self.quit_btn.grid(row=3, column=0)
+        self.quit_btn = Button(root, text="Quit", command=root.destroy, style="P.TButton")
+        self.quit_btn.grid(row=3, column=0, pady=20)
 
-        self.restart_btn = Button(root, text="Restart", command=self.reset)
-        self.restart_btn.grid(row=3, column=4)
+        self.restart_btn = Button(root, text="Restart", command=self.reset, style="P.TButton")
+        self.restart_btn.grid(row=3, column=4, pady=20)
 
     # ----------- LOGIC -------------- #
 
@@ -157,7 +159,6 @@ class TypingTest:
         self.typer.focus()
         self.typer.config(state="normal")
         self.typer.bind("<KeyPress>", self.start)
-        self.typer.bind("<BackSpace>", self.backspace)
         self.words.config(state="normal")
         # Creating a random word list and string of all words put together.
         # You need both forms to do different functions.
@@ -182,7 +183,6 @@ class TypingTest:
 
     def check_char(self, event):
         char_typed = event.char.lower()
-        print(char_typed)
         if char_typed == " ":
             self.check_word()
 
@@ -190,18 +190,17 @@ class TypingTest:
             # Compares the character typed to the particular character in the string of words
             # If it is the same, it highlights the character blue
             self.words.tag_add("correct_char", f"1.{self.position}", f"1.{self.position + 1}")
-            self.words.tag_config("correct_char", background="blue", foreground="white")
+            self.words.tag_config("correct_char", background="#7C9BE8", foreground="white")
         else:
             # If it is different, it highlights the character red
             self.words.tag_add("incorrect_char", f"1.{self.position}", f"1.{self.position + 1}")
-            self.words.tag_config("incorrect_char", background="red", foreground="white")
+            self.words.tag_config("incorrect_char", background="#B7583D", foreground="white")
         self.position += 1
 
     def backspace(self):
         # Conditions for backspace: When you typed the first character in a word
         # The second condition accounts for the very first word
-        print("backspace")
-        if self.position > self.milestone+1 or self.position == 1:
+        if self.position > self.milestone + 1 or self.position == 1:
             self.position -= 1
             self.words.tag_remove("correct_char", f"1.{self.position}", f"1.{self.position + 1}")
             self.words.tag_remove("incorrect_char", f"1.{self.position}", f"1.{self.position + 1}")
@@ -221,23 +220,24 @@ class TypingTest:
         self.position = -1
 
         # Calculate new position based on how many words have been attempted
-        for i in range(self.word_number+1):
+        for i in range(self.word_number + 1):
             self.position += len(word_list[i]) + 1
 
-        #self.milestone is purely for the backspace method
+        # self.milestone is purely for the backspace method
         self.milestone = self.position * 1
 
         if input_word == word_to_check_against:
             # Highlight the correct word blue (by highlighting the characters
             # from beginning of word to current position)
-            self.words.tag_add("correct_char", f"1.{self.position - len(word_list[self.word_number])}", f"1.{self.position}")
-            self.words.tag_config("correct_char", background="blue", foreground="white")
+            self.words.tag_add("correct_char", f"1.{self.position - len(word_list[self.word_number])}",
+                               f"1.{self.position}")
+            self.words.tag_config("correct_char", background="#7C9BE8", foreground="white")
             self.update_score()
 
         else:
             self.words.tag_add("incorrect_char", f"1.{self.position - len(word_list[self.word_number])}",
                                f"1.{self.position}")
-            self.words.tag_config("incorrect_char", background="red", foreground="white")
+            self.words.tag_config("incorrect_char", background="#B7583D", foreground="white")
 
         self.words.see(f"1.{self.position + 50}")
         self.typer.delete(0, "end")
@@ -262,7 +262,5 @@ class TypingTest:
         self.recent_wpm.insert(0, f"{'{:.1f}'.format(self.wpm_score)}")
 
 
-
 typing = TypingTest()
 root.mainloop()
-
